8dbf9308f66574b11e01afa6bd43c74d6dc99904
lcantillo84 <liliannecantillo@yahoo.es>
2022-03-03 16:25:27 -0500

    Started the dashboard page design
    Dashboard and design

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Color } from "ng2-charts";
+import { Chart } from "chart.js";
 
 @Component({
   selector: 'app-dashboard',
@@ -6,7 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
+  chartData = [
+    {
+      data: [90, 10],
+      label: "Account A",
+    },
+  ];
+  chartLabels = [" 90 %"];
+  colors: Color[] = [
+    {
+      backgroundColor: ["#999999", "#C0C2C4"],
+    },
+  ];
+  chartOptions = {
+    responsive: true,
+  };
   constructor() { }
 
   ngOnInit(): void {
